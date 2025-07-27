8f012a3079b75422fbb447141556a268d4c6f770
Athul Nalupurakkal <athulonline13@gmail.com>
2025-07-27 07:16:27 +0530

    intergrted auth0 athentication

---
+++ WeatherApp/Controllers/AdminController.cs
@@ -0,0 +1,48 @@
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using System.Security.Claims;
+using WeatherApp.IService;
+using WeatherApp.Models;
+
+namespace WeatherApp.Controllers
+{
+    public class AdminController : Controller
+    {
+        private readonly ILogger<AdminController> _logger;
+        private readonly IWeatherService _weatherService;
+
+        public AdminController(ILogger<AdminController> logger, IWeatherService weatherService)
+        {
+            _logger = logger;
+            _weatherService = weatherService;
+        }
+
+
+        public IActionResult Login(string returnUrl = "/")
+        {
+            return Challenge(new AuthenticationProperties { RedirectUri = returnUrl }, "Auth0");
+        }
+
+
+        public IActionResult Logout()
+        {
+            return SignOut(
+                new AuthenticationProperties { RedirectUri = "/" },
+                CookieAuthenticationDefaults.AuthenticationScheme,
+                "Auth0");
+        }
+
+        [Authorize]
+        public ActionResult Dashboard() => View();
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
--- WeatherApp/Program.cs
+++ WeatherApp/Program.cs
@@ -1,25 +1,41 @@
+﻿using Auth0.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
 using WeatherApp.IService;
 using WeatherApp.Service;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// Add services to the container.  
 builder.Services.AddControllersWithViews();
 
-//Register DI Services
+// Register DI Services  
 builder.Services.AddScoped<IWeatherService, WeatherService>();
 builder.Services.AddHttpClient("weatherClient", client =>
 {
     client.BaseAddress = new Uri("https://api.openweathermap.org/");
 });
 
+builder.Services.AddAuth0WebAppAuthentication(options =>
+{
+    options.Domain = builder.Configuration["Auth0:Domain"];
+    options.ClientId = builder.Configuration["Auth0:ClientId"];
+    options.ClientSecret = builder.Configuration["Auth0:ClientSecret"];
+});
+
+// Configure cookie settings separately  
+builder.Services.Configure<CookieAuthenticationOptions>(CookieAuthenticationDefaults.AuthenticationScheme, options =>
+{
+    options.LoginPath = "/admin/login";
+    options.AccessDeniedPath = "/access-denied"; // optional  
+});
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// Configure the HTTP request pipeline.  
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.  
     app.UseHsts();
 }
 
@@ -28,6 +44,7 @@ app.UseStaticFiles();
 
 app.UseRouting();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllerRoute(
